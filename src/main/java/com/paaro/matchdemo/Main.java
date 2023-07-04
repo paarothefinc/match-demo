d8d8e7d22aa90b587d32bd536ab9d075301460f9
ppogacar <ppogacar@beenius.tv>
2023-07-04 22:12:09 +0200

    - introduced DemoFixedSizeBatchWithUniqueTimestamps implementation, which is single threaded with fixed batch size and every event with a unique Java generated timestamp - changed 5/10/100/200 batch/bag sizes in test to 100/200/500/100 and in case of DemoFixedSizeBatchWithUniqueTimestamps to 100/1000/10000/100000

--- src/main/java/com/paaro/matchdemo/Main.java
+++ src/main/java/com/paaro/matchdemo/Main.java
@@ -4,19 +4,12 @@ import com.paaro.matchdemo.impl.DemoFixedSizeBag;
 import com.paaro.matchdemo.impl.DemoFixedSizeBagMultiThreaded;
 import com.paaro.matchdemo.impl.DemoFixedSizeBagWithStoredProcedure;
 import com.paaro.matchdemo.impl.DemoFixedSizeBagWithStoredProcedureMultiThreaded;
+import com.paaro.matchdemo.impl.DemoFixedSizeBatchWithUniqueTimestamps;
 import com.paaro.matchdemo.impl.DemoMaximumBatchSizeWithUniqueMatchIds;
 
 public class Main {
     public static void main(final String[] args) {
         try (final DemoMaximumBatchSizeWithUniqueMatchIds demo = new DemoMaximumBatchSizeWithUniqueMatchIds()) {
-            demo.run(5, true);
-        }
-
-        try (final DemoMaximumBatchSizeWithUniqueMatchIds demo = new DemoMaximumBatchSizeWithUniqueMatchIds()) {
-            demo.run(10, true);
-        }
-
-        try (final DemoMaximumBatchSizeWithUniqueMatchIds demo = new DemoMaximumBatchSizeWithUniqueMatchIds()) {
             demo.run(100, true);
         }
 
@@ -24,12 +17,12 @@ public class Main {
             demo.run(200, true);
         }
 
-        try (final DemoFixedSizeBag demo = new DemoFixedSizeBag()) {
-            demo.run(5, true);
+        try (final DemoMaximumBatchSizeWithUniqueMatchIds demo = new DemoMaximumBatchSizeWithUniqueMatchIds()) {
+            demo.run(500, true);
         }
 
-        try (final DemoFixedSizeBag demo = new DemoFixedSizeBag()) {
-            demo.run(10, true);
+        try (final DemoMaximumBatchSizeWithUniqueMatchIds demo = new DemoMaximumBatchSizeWithUniqueMatchIds()) {
+            demo.run(1000, true);
         }
 
         try (final DemoFixedSizeBag demo = new DemoFixedSizeBag()) {
@@ -40,12 +33,12 @@ public class Main {
             demo.run(200, true);
         }
 
-        try (final DemoFixedSizeBagWithStoredProcedure demo = new DemoFixedSizeBagWithStoredProcedure()) {
-            demo.run(5, true);
+        try (final DemoFixedSizeBag demo = new DemoFixedSizeBag()) {
+            demo.run(500, true);
         }
 
-        try (final DemoFixedSizeBagWithStoredProcedure demo = new DemoFixedSizeBagWithStoredProcedure()) {
-            demo.run(10, true);
+        try (final DemoFixedSizeBag demo = new DemoFixedSizeBag()) {
+            demo.run(1000, true);
         }
 
         try (final DemoFixedSizeBagWithStoredProcedure demo = new DemoFixedSizeBagWithStoredProcedure()) {
@@ -56,12 +49,12 @@ public class Main {
             demo.run(200, true);
         }
 
-        try (final DemoFixedSizeBagMultiThreaded demo = new DemoFixedSizeBagMultiThreaded()) {
-            demo.run(5, true);
+        try (final DemoFixedSizeBagWithStoredProcedure demo = new DemoFixedSizeBagWithStoredProcedure()) {
+            demo.run(500, true);
         }
 
-        try (final DemoFixedSizeBagMultiThreaded demo = new DemoFixedSizeBagMultiThreaded()) {
-            demo.run(10, true);
+        try (final DemoFixedSizeBagWithStoredProcedure demo = new DemoFixedSizeBagWithStoredProcedure()) {
+            demo.run(1000, true);
         }
 
         try (final DemoFixedSizeBagMultiThreaded demo = new DemoFixedSizeBagMultiThreaded()) {
@@ -72,16 +65,12 @@ public class Main {
             demo.run(200, true);
         }
 
-        try (
-            final DemoFixedSizeBagWithStoredProcedureMultiThreaded demo =
-                new DemoFixedSizeBagWithStoredProcedureMultiThreaded()) {
-            demo.run(5, true);
+        try (final DemoFixedSizeBagMultiThreaded demo = new DemoFixedSizeBagMultiThreaded()) {
+            demo.run(500, true);
         }
 
-        try (
-            final DemoFixedSizeBagWithStoredProcedureMultiThreaded demo =
-                new DemoFixedSizeBagWithStoredProcedureMultiThreaded()) {
-            demo.run(10, true);
+        try (final DemoFixedSizeBagMultiThreaded demo = new DemoFixedSizeBagMultiThreaded()) {
+            demo.run(1000, true);
         }
 
         try (
@@ -94,6 +83,34 @@ public class Main {
             final DemoFixedSizeBagWithStoredProcedureMultiThreaded demo =
                 new DemoFixedSizeBagWithStoredProcedureMultiThreaded()) {
             demo.run(200, true);
+        }
+
+        try (
+            final DemoFixedSizeBagWithStoredProcedureMultiThreaded demo =
+                new DemoFixedSizeBagWithStoredProcedureMultiThreaded()) {
+            demo.run(500, true);
+        }
+
+        try (
+            final DemoFixedSizeBagWithStoredProcedureMultiThreaded demo =
+                new DemoFixedSizeBagWithStoredProcedureMultiThreaded()) {
+            demo.run(1000, true);
+        }
+
+        try (final DemoFixedSizeBatchWithUniqueTimestamps demo = new DemoFixedSizeBatchWithUniqueTimestamps()) {
+            demo.run(100, true);
+        }
+
+        try (final DemoFixedSizeBatchWithUniqueTimestamps demo = new DemoFixedSizeBatchWithUniqueTimestamps()) {
+            demo.run(1000, true);
+        }
+
+        try (final DemoFixedSizeBatchWithUniqueTimestamps demo = new DemoFixedSizeBatchWithUniqueTimestamps()) {
+            demo.run(10000, true);
+        }
+
+        try (final DemoFixedSizeBatchWithUniqueTimestamps demo = new DemoFixedSizeBatchWithUniqueTimestamps()) {
+            demo.run(100000, true);
         }
     }
 }
---
+++ src/main/java/com/paaro/matchdemo/impl/DemoFixedSizeBatchWithUniqueTimestamps.java
@@ -0,0 +1,53 @@
+package com.paaro.matchdemo.impl;
+
+import com.paaro.matchdemo.model.Event;
+
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DemoFixedSizeBatchWithUniqueTimestamps extends DemoBase {
+    public void run(final int batchSize, final boolean truncateTableAfterRun) {
+        outputHeader("Fixed batch size with unique timestamps", batchSize);
+
+        try (final PreparedStatement statement = CONNECTION.prepareStatement(SQL_INSERT_WITH_TIMESTAMP)) {
+            int eventCounter = 0;
+
+            // counter of DB calls needed to be made
+            int dbCallCounter = 0;
+
+            for (String line = READER.readLine(); line != null; line = READER.readLine()) {
+                final Event event = new Event(line);
+
+                try {
+                    // executes batch when batchSize is reached
+                    if (++eventCounter % batchSize == 0) {
+                        statement.executeBatch();
+                        dbCallCounter++;
+
+                        // since we flushed the batch, reset eventCounter
+                        eventCounter = 0;
+                    }
+
+                    // create statement for the event with the unique timestamp
+                    setStatementParametersWithInsertTimestamp(statement, event, getUniqueTimestamp());
+
+                    // add the statement to the batch
+                    statement.addBatch();
+                } catch (final SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+            // at the end, flush possible remaining events
+            statement.executeBatch();
+            dbCallCounter++;
+
+            // output summary
+            verifyResult(dbCallCounter, truncateTableAfterRun);
+        } catch (final SQLException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
